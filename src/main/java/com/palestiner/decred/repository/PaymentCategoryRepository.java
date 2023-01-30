cc4a5f31129ce6e26213a4d27034f1e88c1d3093
KorPaEv <korpaev@gmail.com>
2023-01-30 09:01:26 +0700

    Refactoring repositories
    Add Query and custom method 'findByName' for UserRepository

--- src/main/java/com/palestiner/decred/repository/PaymentCategoryRepository.java
+++ src/main/java/com/palestiner/decred/repository/PaymentCategoryRepository.java
@@ -2,7 +2,11 @@ package com.palestiner.decred.repository;
 
 import com.palestiner.decred.model.PaymentCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
+@Repository
+@Transactional
 public interface PaymentCategoryRepository extends JpaRepository<PaymentCategory, Long> {
 
 }
--- src/main/java/com/palestiner/decred/repository/UserRepository.java
+++ src/main/java/com/palestiner/decred/repository/UserRepository.java
@@ -2,8 +2,16 @@ package com.palestiner.decred.repository;
 
 import com.palestiner.decred.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
-
+@Repository
+@Transactional
 public interface UserRepository extends JpaRepository<User, Integer> {
+
+    @Query(nativeQuery = true, value = "select user_name from decred_user u where u.user_name = :name")
+    User findByName(@Param("name") String name);
 
 }
