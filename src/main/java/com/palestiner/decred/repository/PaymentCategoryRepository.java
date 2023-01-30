251d149fb57a103c0e7ff0ea741f9680d56070c0
KorPaEv <korpaev@gmail.com>
2023-01-30 10:02:44 +0700

    Add PaymentCategoryService PaymentCategoryServiceImpl
    Refactor
    PaymentCategoryRepository

--- src/main/java/com/palestiner/decred/repository/PaymentCategoryRepository.java
+++ src/main/java/com/palestiner/decred/repository/PaymentCategoryRepository.java
@@ -1,12 +1,19 @@
 package com.palestiner.decred.repository;
 
 import com.palestiner.decred.model.PaymentCategory;
+import com.palestiner.decred.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 @Repository
 @Transactional
 public interface PaymentCategoryRepository extends JpaRepository<PaymentCategory, Long> {
 
+    @Query(nativeQuery = true, value = "select name from payment_category pc where pc.name = :name")
+    Optional<PaymentCategory> findByName(@Param("name") String name);
 }
---
+++ src/main/java/com/palestiner/decred/service/PaymentCategoryService.java
@@ -0,0 +1,24 @@
+package com.palestiner.decred.service;
+import com.palestiner.decred.model.PaymentCategory;
+import java.util.List;
+import java.util.Optional;
+
+public interface PaymentCategoryService {
+
+    PaymentCategory addNew(PaymentCategory paymentCategory);
+
+    List<PaymentCategory> getAll();
+
+    void removeAll();
+
+    void removePaymentCategory(PaymentCategory paymentCategory);
+
+    void removePaymentCategory(Long paymentCategoryId);
+
+    PaymentCategory update(PaymentCategory paymentCategory);
+
+    Optional<PaymentCategory> getPaymentCategory(String name);
+
+    Optional<PaymentCategory> getPaymentCategory(Long paymentCategoryId);
+
+}
