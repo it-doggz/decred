e7aacd448f8c99302f3156b77f4d1cbfc6bd19ab
KorPaEv <korpaev@gmail.com>
2023-03-17 00:11:32 +0700

    Fix PaymentCategoryEntityRepositoryTest Add findByNameIsNotNullTest

--- src/test/java/com/palestiner/decred/jpa/PaymentCategoryEntityRepositoryTest.java
+++ src/test/java/com/palestiner/decred/jpa/PaymentCategoryEntityRepositoryTest.java
@@ -9,24 +9,33 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.jdbc.EmbeddedDatabaseConnection;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.jdbc.Sql;
 
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 
 @DataJpaTest
 @AutoConfigureTestDatabase(connection = EmbeddedDatabaseConnection.H2)
 public class PaymentCategoryEntityRepositoryTest {
 
-    private final String PAYMENT_CATEGORY_NAME = "TESTNAME";
+    private final String PAYMENT_CATEGORY_NAME = "products";
 
     @Autowired
     private PaymentCategoryRepository paymentCategoryRepository;
+
+    @Test
+    @Sql("classpath:init.sql")
+    public void findByNameIsNotEmptyTest() {
+        Optional<PaymentCategory> paymentCategoryOptional = paymentCategoryRepository.findByName(PAYMENT_CATEGORY_NAME);
+        assertFalse(paymentCategoryOptional.isEmpty());
+        paymentCategoryOptional.ifPresent(paymentCategory -> assertThat(paymentCategory.getName()).isEqualTo(PAYMENT_CATEGORY_NAME));
+    }
 
     @Test
     public void findByNameIsEmptyTest() {
         Optional<PaymentCategory> paymentCategory = paymentCategoryRepository.findByName(PAYMENT_CATEGORY_NAME);
         assertThat(paymentCategory).isEmpty();
     }
-
 }
