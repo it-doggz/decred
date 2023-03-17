e977ca99b9b551746fe88cf2eec6cc7d8516739d
KorPaEv <korpaev@gmail.com>
2023-03-17 11:39:27 +0700

    Fix for user list

--- src/main/java/com/palestiner/decred/repository/UserRepository.java
+++ src/main/java/com/palestiner/decred/repository/UserRepository.java
@@ -4,11 +4,12 @@ import com.palestiner.decred.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-import java.util.Optional;
+
+import java.util.List;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
 
     @Query(nativeQuery = true, value = "select * from decred_user u where u.user_name = :userName")
-    Optional<User> findByName(@Param("userName") String userName);
+    List<User> findByName(@Param("userName") String userName);
 
 }
